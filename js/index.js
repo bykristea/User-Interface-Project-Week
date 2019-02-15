8e0334d61e7afdd6bb3d2819208dbff6cb2319f8
Kristea Gamel <krgamel@gmail.com>
2019-02-14 20:56:30 -0600

    created click event for nav to expand with animation slide in

--- js/index.js
+++ js/index.js
@@ -1,1 +1,79 @@
 // JS goes here
+
+class NavLink {
+    constructor(link) {
+        this.link = link;
+        this.link.addEventListener('click', toggleNavClose)
+
+        this.link.addEventListener('mouseenter', () => this.select());
+
+        navLinks.addEventListener('mouseleave', () => this.deselect());
+    };
+
+    select() {
+        document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('nav-link-selected'));
+        this.link.classList.add('nav-link-selected');
+    }
+    deselect() {
+        document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('nav-link-selected'));
+        document.querySelector('.nav-link-default').classList.add('nav-link-selected');
+    }
+}
+
+const toggleNavOpen = () => {
+    navOpen.classList.toggle('nav-open-toggle');
+    navClose.classList.toggle('nav-close-toggle');
+    TweenMax.from(nav, 2, {
+        ease: Power2.easeOut,
+        height: "0",
+        width: "0",
+        right: "0",
+        top: "0",
+        fontSize: "0",
+        lineHeight: "0"
+    });
+    TweenMax.to(nav, 2, {
+        ease: Power2.easeOut,
+        height: "100%",
+        width: "100%",
+        top: "0",
+        position: "fixed",
+        fontSize: "48",
+        lineHeight: "2",
+    });
+    nav.classList.toggle('nav-toggle');
+}
+
+const toggleNavClose = () => {
+    navOpen.classList.toggle('nav-open-toggle');
+    navClose.classList.toggle('nav-close-toggle');
+    TweenMax.from(nav, 1, {
+        ease: Power2.easeIn,
+        height: "100%",
+        width: "100%",
+        top: "0",
+        position: "fixed",
+        fontSize: "48",
+        lineHeight: "2"
+    });
+    TweenMax.to(nav, 1, {
+        ease: Power2.easeIn,
+        height: "0",
+        width: "0",
+        right: "0",
+        top: "0",
+        fontSize: "0",
+        lineHeight: "0",
+        onComplete: () => nav.classList.toggle('nav-toggle')
+    });
+}
+
+const navOpen = document.querySelector('.nav-open');
+const navClose = document.querySelector('.nav-close');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelector('.nav-links');
+const navLink = document.querySelectorAll('.nav-link');
+
+navOpen.addEventListener('click', toggleNavOpen);
+navClose.addEventListener('click', toggleNavClose);
+navLink.forEach(navLink => new NavLink(navLink));
